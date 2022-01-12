6fece53c8d2eeb5320472e3ccabd19e7508a5144
lukemarohn <lukemarohn@gmail.com>
2022-01-12 00:57:37 -0800

    improve grid on mobile

--- components/MovingGrid.js
+++ components/MovingGrid.js
@@ -20,6 +20,7 @@ export const MovingGrid = ({translateXOffset = 0, direction = 1, color = "#E53E3
     return <Box 
             ref={scrollTopRef}
             className="bg"
+            width="200vw"
             style={{
                 "--translateX": translateX + 'px',
                 "-webkit-mask-image": "-webkit-gradient(linear, left 90%, left top, color-stop(0%, rgba(0,0,0,0)), color-stop(10%, rgba(0,0,0,1)), color-stop(100%, rgba(0,0,0,0)) )",
@@ -28,13 +29,13 @@ export const MovingGrid = ({translateXOffset = 0, direction = 1, color = "#E53E3
                 "maskImage": "gradient(linear, left 90%, left top, from(rgba(0,0,0,1)), to(rgba(0,0,0,0)))"
             }}
             transform={`perspective(200px) rotateX(20deg) scale(3,1) translateZ(0) translateX(var(--translateX))`}
-            content="''" display="block" position="absolute" bottom={0} left={0} right={0} width="100%" height="100%"
+            content="''" display="block" position="absolute" bottom={0} left={"-50%"} right={0} width="200%" height="100%"
             padding={1}
             outline="1px solid transparent"
             transformOrigin="bottom center"
             willChange="transform"
             backgroundPosition="center center"
             backgroundSize="40px 40px"
-            backgroundImage={`linear-gradient(to right, ${color} 1px, transparent 2px), linear-gradient(to bottom, ${color} 1px, transparent 2px)`}
+            backgroundImage={`linear-gradient(to right, ${color} 1px, transparent 1px), linear-gradient(to bottom, ${color} 1px, transparent 2px)`}
         ></Box>
 }
