9bcbd6f43bdb3bdb134d9e542d5a734691930615
lukemarohn <lukemarohn@gmail.com>
2022-01-12 00:43:01 -0800

    try max height

--- components/SectionBanner.js
+++ components/SectionBanner.js
@@ -61,7 +61,7 @@ export const SectionBanner = ({}) => {
 
            {/* Background Image and Gradients */}
            <Box gridColumn={1} gridRow={1} w="100%" zIndex={0}>
-                <AspectRatio ratio={16 / 9} w="100%" className="appHeight" position="relative">
+                <AspectRatio ratio={[ 9/16, null, 1]} maxH="100vh" w="100%" position="relative">
                     <Box>
                         <Box bgGradient="linear-gradient(black, transparent)" w="100%" h="200px" zIndex="1" marginBottom="auto" position="absolute" inset="0" />
                         <Box bgGradient="linear-gradient(transparent, black)" w="100%" h="50vh" zIndex="1" marginTop="auto" position="absolute" inset="0" />
--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,8 @@
-import { ChakraProvider, extendTheme, Box } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import "../styles/globals.css"
-import { useEffect} from 'react'
 import { theme } from '../theme.js'
 
 function MyApp({ Component, pageProps }) {
-
-  const appHeight = () => {
-      const el = document.documentElement
-      el.style.setProperty('--vh', `${window.innerHeight / 100}px`)
-  }
-  useEffect(() => {
-      appHeight()
-      window.addEventListener('resize', appHeight)
-  })
 
   return <ChakraProvider theme={extendTheme(theme)}>
     <Component {...pageProps} />
